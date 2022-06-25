c6e6d25fba843801102e8df52f3f5db858bf77ca
simon-tanna <gcab022109@coderacademy.edu.au>
2022-06-25 12:50:56 +1000

    inserted conditional rendering on user login status in navigation

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,15 @@ import Message from "./Message";
 import MessageForm from "./MessageForm";
 import Navigation from "./Navigation";
 import Login from "./Login";
+import React, { useState } from "react";
 
 function App() {
+	const [loggedInUser, setLoggedInUser] = useState("");
+
 	return (
 		<div className="App">
 			<h1>Breaddit</h1>
-			<Navigation />
+			<Navigation loggedInUser={loggedInUser} />
 			<Messages />
 			<Message />
 			<MessageForm />
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-function Navigation() {
+function Navigation({ loggedInUser }) {
 	return (
 		<nav>
 			<a href="/">Home</a>
 			<a href="/">About</a>
-			<a href="/">Login</a>
-			<a href="/">Sign Up</a>
+			{loggedInUser ? (
+				<>
+					<a href="/">Log Out</a>
+					{loggedInUser}
+				</>
+			) : (
+				<>
+					<a href="/">Login</a>
+					<a href="/">Sign Up</a>
+				</>
+			)}
 		</nav>
 	);
 }
