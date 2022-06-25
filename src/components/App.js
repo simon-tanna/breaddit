9ade83a498fd8e85eb5ee1651fd354334de47c78
simon-tanna <gcab022109@coderacademy.edu.au>
2022-06-25 15:21:47 +1000

    added submit methods and state to message form

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,30 @@
 import Messages from "./Messages";
-import Message from "./Message";
 import MessageForm from "./MessageForm";
 import Navigation from "./Navigation";
 import Login from "./Login";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import seedMessages from "../data/message-seed.json";
 
 function App() {
 	const [loggedInUser, setLoggedInUser] = useState("");
+	const [messageList, setMessageList] = useState([]);
 
 	const activateUser = (username) => {
 		setLoggedInUser(username);
 	};
 
+	useEffect(() => {
+		// API fetch to go here
+		setMessageList(seedMessages);
+	}, []);
+
 	return (
 		<div className="App">
 			<h1>Breaddit</h1>
 			<Navigation loggedInUser={loggedInUser} activateUser={activateUser} />
-			<Messages />
-			<Message />
-			<MessageForm />
-			<Login activateUser={activateUser} />
+			{!loggedInUser && <Login activateUser={activateUser} />}
+			{loggedInUser && <MessageForm />}
+			<Messages messageList={messageList} />
 		</div>
 	);
 }
